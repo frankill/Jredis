700f50e8733e58585daeff03e562c512ddaa4ba9
frankill <34159384+frankill@users.noreply.github.com>
2018-11-12 15:31:10 +0800

    Update Jredis.jl

--- src/Jredis.jl
+++ src/Jredis.jl
@@ -2,4 +2,14 @@ using Jredis
     
     using Sockets 
 
+    # Connection commands
+    export auth, echo, ping, quit, select
+    # list commands
+    export blpop, brpop, brpoplpush, lindex, linsert, llen,
+           lpop, lpush, lpushx, lrange, lrem, lset,
+           ltrim, rpop, rpoplpush, rpush, rpushx
+
+    include("connection.jl") 
+    include("redis.jl")
+    include("commands.jl")
 end 
