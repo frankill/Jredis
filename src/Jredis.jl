096cb377db518871b7629f364535e3385f568c42
frankill <34159384+frankill@users.noreply.github.com>
2018-12-07 23:15:43 +0800

    Update Jredis.jl

--- src/Jredis.jl
+++ src/Jredis.jl
@@ -5,7 +5,7 @@ module Jredis
     using Sockets 
     import JSON.json
     
-    export RedisConnection, disconnect,read_pipeline,@pipelines
+    export RedisConnection, disconnect,read_pipeline, @pipelines, @transaction
     # Key commands
     export del,  exists, expire, expireat,
            migrate, move, persist, pexpire, pexpireat,
