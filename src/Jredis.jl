b1fd0c1b3fb8ff5e6a8e627a6359d2c3f67ff933
frankill <34159384+frankill@users.noreply.github.com>
2018-12-07 11:29:51 +0800

    Update Jredis.jl

--- src/Jredis.jl
+++ src/Jredis.jl
@@ -24,20 +24,15 @@ module Jredis
         socket::TCPSocket
     end
 
-    function RedisConnection(; host="127.0.0.1", port=6379, password= "", db=0)
+    function RedisConnection(; host="127.0.0.1", port=6379, password= nothing, db=0)
+        try
             socket = connect(host, port)
             connection = RedisConnection(host, port, password, db, socket)
             on_connect(connection)
+        catch
+            throw("Failed to connect to Redis server")
+        end
     end
-#     function RedisConnection(; host="127.0.0.1", port=6379, password= nothing, db=0)
-#         try
-#             socket = connect(host, port)
-#             connection = RedisConnection(host, port, password, db, socket)
-#             on_connect(connection)
-#         catch
-#             throw("Failed to connect to Redis server")
-#         end
-#     end
 
     function on_connect(conn::RedisConnectionBase)
         conn.password != "" && auth(conn, conn.password)
