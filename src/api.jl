8159a25a16e3f1b160d3595fec6dc92657c47993
frankill <34159384+frankill@users.noreply.github.com>
2018-12-14 10:42:03 +0800

    Update api.jl

--- src/api.jl
+++ src/api.jl
@@ -87,7 +87,7 @@ function pack_command(command::AbstractVector)
     packed_command
 end
 
-@inline Merge_parameters(command::Vector) = vcat(map(Merge_parameter, command)...)
+@inline Merge_parameters(command::Vector) = vcat(map(Merge_parameter, command)...)::Vector{String}
 
 Merge_parameter(token::Symbol) = string(token)
 Merge_parameter(token::Number) = string(token)
