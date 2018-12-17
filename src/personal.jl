333ed0d44635e3b2b8204749950c2ad73108ff69
frankill <34159384+frankill@users.noreply.github.com>
2018-12-17 15:09:51 +0800

    Update personal.jl

--- src/personal.jl
+++ src/personal.jl
@@ -19,7 +19,7 @@ function redis_collect(conn::RedisConnection , data::Vector = [])
 		if ! (syms in sym) 
 		    push!(data, tmp)
 		else 
-		    push!(data, reply( conn.socket  ) 
+		    push!(data, reply( conn.socket  ))
 		end 
 
 		redis_collect(conn, data)
